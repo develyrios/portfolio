3b8e06b1bd01bde45376792d91c75c9f6907fd51
Bev Feitosa <bevely1407@gmail.com>
2023-01-26 09:18:19 -0300

    <feat>:adiciona react e suas dependências
    dependências: styled components, react router dom, axios e react icons

---
+++ portfolio-bevfeitosa/src/App.jsx
@@ -0,0 +1,11 @@
+import GlobalStyle from './styles/GlobalStyle'
+import { Header } from './components/Header'
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
+  )
+}

9e4a421895310df6dbd22e0def71c459da0e5097
Bev Feitosa <bevely1407@gmail.com>
2023-01-26 16:28:04 -0300

    <feat>:adiciona componente footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import { Title1 } from "../../styles/Text"
+import { Container } from "./style"
+
+export const Footer = () => {
+    return (
+        <Container className="border-major border-shadow-major">
+            <Title1>Desenhado e desenvolvido por Bev Feitosa</Title1>
+        </Container>
+    )
+}
---
+++ src/components/Footer/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    grid-column: 5 / span 4;
+    padding: 8px;
+`

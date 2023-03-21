f601e421328fd827f85cf87dfa11ea728fc95bd2
Bev Feitosa <bevely1407@gmail.com>
2023-03-21 10:20:06 -0300

    <feat>:atualiza a bio

--- src/components/Bio/index.jsx
+++ src/components/Bio/index.jsx
@@ -6,19 +6,22 @@ export const Bio = () => {
         <Container className="border-major border-shadow-major">
             <Title1>Sobre</Title1>
             <Paragraph>
-                Meu foco atual é uma colocação no mercado de 
-                Desenvolvimento Front-end  como trainee ou junior, 
-                tanto remoto como presencial.
+                Tenho formação em Engenharia de Materiais e meu foco atual é 
+                colocação no mercado de Desenvolvimento Front-end como trainee 
+                ou junior tanto remoto como presencial.
             </Paragraph>
             <Paragraph>
                 Gosto de resolver problemas, trabalhar em equipe, aprender 
                 coisas novas e aprimorar o que já sei. Faço uso de ferramentas 
-                como Git, Github, Figma, VS Code e Trello. Adoro criar e 
+                como Git, Github, Figma e Trello, além das linguagens descritas
+                na seção "Tecnologias". Também gosto de criar e 
                 desenvolver ideias, fazendo elas saírem do papel e aprimorá-las 
                 sempre que preciso.
             </Paragraph>
             <Paragraph>
-                Amo me comunicar com pessoas e compartilhar meus processos produtivos e resultados.
+                Estou há cerca de um ano estudando desenvolvimento front-end. Durante 
+                minha jornada já fiz diversos cursos, aplicando sempre que possível os 
+                conhecimentos adquiridos em projetos pessoais e no meu portfólio.
             </Paragraph>
         </Container>
     )

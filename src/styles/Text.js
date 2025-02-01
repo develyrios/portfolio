68dcb963c74cf17cbf43e209701cd95325e74ece
Bev Feitosa <bevfeitosa@gmail.com>
2025-02-01 18:57:13 -0300

    <refactor>: refatora os componentes de texto
    Altera o nome de alguns componentes para o idioma Português. Adiciona os font-families dentro dos componentes, que antes eram classes separadas.

--- src/styles/Text.js
+++ src/styles/Text.js
@@ -1,24 +1,31 @@
 import styled from "styled-components";
 
-export const Title1 = styled.h1`
+export const Titulo1 = styled.h1`
+  font-family: var(--montserrat);
   font-weight: 800;
   font-size: 4rem;
   text-align: center;
+  text-transform: uppercase;
+
+  grid-column: 1 / -1;
 `;
 
-export const Title2 = styled.h2`
+export const Titulo2 = styled.h2`
+  font-family: var(--poppins);
   font-weight: 500;
   font-size: 2rem;
   text-align: left;
 `;
 
-export const Paragraph = styled.p`
+export const Paragrafo = styled.p`
+  font-family: var(--nunito);
   font-weight: 500;
   font-size: 1rem;
   text-align: left;
 `;
 
 export const Span = styled.span`
+  font-family: var(--nunito);
   font-size: 1.5rem;
   font-weight: 500;
 `;
